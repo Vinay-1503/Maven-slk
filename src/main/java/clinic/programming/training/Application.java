1261644e4e5b15968374546f302e3c11badbc257
example-git-vinay13 <example-git-student@chintalapativinay813@gmail.com>
2025-06-16 22:41:33 +0530

    new string utils added

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,12 +1,13 @@
 package clinic.programming.training;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
-    public void greet()
-    {
-        System.err.println("naid");
+    public void greet() {
+        System.out.println("naid");
     }
+
     public Application() {
-        System.out.println ("Inside Application");
+        System.out.println("Inside Application");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
@@ -14,5 +15,15 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
     app.greet();
+    
+    String testString = "   Hello Maven!   ";
+
+    // Check if the string is blank
+    if (StringUtils.isBlank(testString)) {
+        System.out.println("The string is blank.");
+    } else {
+        // Trim the string and print it
+        System.out.println("Trimmed string: '" + StringUtils.trim(testString) + "'");
+    }
     }
 }
